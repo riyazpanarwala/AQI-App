6f45fd8f45a9adf4a50d7295ae4c49def913c52b
Riyazahmed Panarwala <35068273+riyazpanarwala@users.noreply.github.com>
2025-12-03 15:48:37 +0530

    deleted app.json

--- app.config.js
+++ app.config.js
@@ -1,8 +1,48 @@
 import 'dotenv/config';
 
 export default ({ config }) => ({
-    ...config,
-    extra: {
-        waqiToken: process.env.WAQI_TOKEN,
+  ...config,
+
+  name: "aqi-app",
+  slug: "aqi-app",
+  version: "1.0.0",
+
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  newArchEnabled: true,
+
+  splash: {
+    image: "./assets/splash-icon.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff",
+  },
+
+  ios: {
+    supportsTablet: true,
+  },
+
+  android: {
+    package: "com.riyazpanarwala.aqiapp",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
+    edgeToEdgeEnabled: true,
+  },
+
+  web: {
+    favicon: "./assets/favicon.png",
+  },
+
+  extra: {
+    waqiToken: process.env.WAQI_TOKEN,
+	eas: {
+		projectId: process.env.EAS_PROJECT_ID,
+	},
+  },
+
+  cli: {
+    appVersionSource: "remote",
+  }
 });
